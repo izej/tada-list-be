e48e3628138d6dc2b778d646362c06e6e432ee87
Izabela Bożęcka <izabela.bozecka@gmail.com>
2025-05-12 14:57:36 +0200

    init

---
+++ src/main/kotlin/pl/izej/tada_list/TadaListApplication.kt
@@ -0,0 +1,11 @@
+package pl.izej.tada_list
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TadaListApplication
+
+fun main(args: Array<String>) {
+	runApplication<TadaListApplication>(*args)
+}
