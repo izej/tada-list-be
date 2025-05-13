6fc07efeb024c9474a22ecb7ce9e40035af59970
Izabela Bożęcka <izabela.bozecka@gmail.com>
2025-05-13 14:36:34 +0200

    Auth - get current user

--- src/main/kotlin/pl/izej/tadalist/api/v1/controller/UserController.kt
+++ src/main/kotlin/pl/izej/tadalist/api/v1/controller/UserController.kt
@@ -8,14 +8,22 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import pl.izej.tadalist.api.v1.mapper.toDto
 import pl.izej.tadalist.service.UserService
+import java.security.Principal
 
 @RestController
-@RequestMapping("/api/users")
+@RequestMapping("/api/v1/users")
 class UserController(private val userService: UserService) {
 
     @PreAuthorize("hasRole('ROLE_USER')")
     @GetMapping("/{id}")
     fun getById(@PathVariable id: String): UserDto = userService.getById(id).toDto()
+
+    @PreAuthorize("hasRole('ROLE_USER')")
+    @GetMapping("/me")
+    fun getCurrentUser(principal: Principal): UserDto = principal.name.let {
+        userService.findByEmail(it)?.toDto()
+            ?: throw NoSuchElementException("User with email $it not found")
+    }
 
 //    @PreAuthorize("hasRole('ROLE_USER')")
 //    @PutMapping("/{id}/details")
